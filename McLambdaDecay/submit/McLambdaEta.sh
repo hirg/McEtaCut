951f85070b12c4a24d87306bbb70a25a07485bc8
Xu Sun <sunxuhit@gmail.com>
2017-08-28 14:48:38 -0700

    v1.1: update submit macro

--- McLambdaDecay/submit/McLambdaEta.sh
+++ McLambdaDecay/submit/McLambdaEta.sh
@@ -12,12 +12,13 @@ then
   do
     cp ./run.csh ./run$Name${counter}.csh
 
-    echo "mkdir $Name$counter" >> ./run$Name${counter}.csh
-    echo "cp McLambdaEta.C $Name$counter" >> ./run$Name${counter}.csh
-    echo "cd $Name$counter" >> ./run$Name${counter}.csh
+    echo "mkdir -p $Name$counter/code" >> ./run$Name${counter}.csh
+    echo "cp -r ../Utility $Name$counter" >> ./run$Name${counter}.csh
+    echo "cp McLambdaEta.C $Name$counter/code" >> ./run$Name${counter}.csh
+    echo "cd $Name$counter/code" >> ./run$Name${counter}.csh
 
     echo "root4star -b -q -x McLambdaEta.C++'('6','0','0','5000000','$counter')'" >> ./run$Name${counter}.csh
-    echo "cd .." >> ./run$Name${counter}.csh
+    echo "cd ../.." >> ./run$Name${counter}.csh
     echo "rm -rf $Name$counter" >> ./run$Name${counter}.csh
     qsub -hard -l scratchfree=500,h_cpu=2:00:00,h_vmem=1.8G,projectio=1 -o /global/project/projectdirs/starprod/rnc/xusun/OutPut/AuAu$Energy/Log/job$Name$counter.log -e /global/project/projectdirs/starprod/rnc/xusun/OutPut/AuAu$Energy/Log/job$Name$counter.err ./run$Name$counter.csh
     mv ./run$Name${counter}.csh /global/project/projectdirs/starprod/rnc/xusun/OutPut/AuAu$Energy/Script/
